b60bc179a4396574a00603d77ae5a627d795aac0
lx00t <lx00t@nomail.nope>
2022-10-05 18:28:27 +0200

    add saving to the channel config

--- src/FlugChannels.py
+++ src/FlugChannels.py
@@ -5,7 +5,7 @@ import FlugConfig
 import logging
 
 class FlugChannels:
-    _channelConfigPath: dict = None             # store the channel config (pre-stage)
+    _channelConfigPath: dict = None             # store the channel config path
     channelConfig: FlugConfig.FlugConfig = None # store the actual channel config (FlugConfig instance)
 
     def __init__(self, channelConfigPath: str):
@@ -13,23 +13,32 @@ class FlugChannels:
 
         return
 
-    def load(self) -> bool:
+    def loadCfg(self) -> bool:
         # load the config file
         self.channelConfig = FlugConfig.FlugConfig(self._channelConfigPath)
 
         if self.channelConfig.load() != True:
-            logging.critical("Failed to load Channel Config from '%s'!" % self._channelConfigPath)
+            logging.critical("Failed to load Channel-Config from '%s'!" % self._channelConfigPath)
 
             return False
 
-        logging.info("Channel config loaded! It has %d entries!" % len(self.channelConfig.c().keys()))
+        logging.info("Channel-Config loaded! It has %d entries!" % len(self.channelConfig.c().keys()))
+
+        return True
+
+    def save(self) -> bool:
+        # save the config file
+        if self.channelConfig.save() != True:
+            logging.critical("Failed to save Channel-Config to '%s'!" % self._channelConfigPath)
+
+            return False
 
         return True
 
     def isConfigLoaded(self) -> bool:
         return self.channelConfig != None
 
-    def doesChannelExist(self, id: str) -> bool:
+    def isChannelKnown(self, id: str) -> bool:
         return self.channelConfig.c().get(id, None) != None
 
     def getchannelConfig(self, id: str) -> dict:
