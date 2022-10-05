2337bb883f4591559ea45e7960698099ac3dc092
lx00t <lx00t@nomail.nope>
2022-10-05 18:29:13 +0200

    add two modules and clean up the main file

--- src/FlugVogel.py
+++ src/FlugVogel.py
@@ -9,6 +9,9 @@ import FlugConfig
 import FlugLoggerConfig
 import FlugCredentials
 import FlugChannels
+import FlugUsers
+import FlugRoles
+import FlugModules
 import FlugClient
 
 # default locations for the config and token file
@@ -16,6 +19,8 @@ DEFAULT_FLUGVOGEL_CONFIG_PATH = "config.json"
 DEFAULT_FLUGVOGEL_TOKEN_PATH = "token.json"
 
 # default config keys for the main application
+DEFAULT_FLUGVOGEL_CFG_KEY_GUILDID = "guildID"
+
 DEFAULT_FLUGVOGEL_CFG_KEY_LOG_CONFIG = "logConfig"
 DEFAULT_FLUGVOGEL_CFG_KEY_LOG_CONFIG_FORMAT = "logFormat"
 DEFAULT_FLUGVOGEL_CFG_KEY_LOG_CONFIG_LEVEL = "logLevel"
@@ -24,15 +29,22 @@ DEFAULT_FLUGVOGEL_CFG_KEY_LOG_CONFIG_FILE_SIZE = "logFileSize"
 DEFAULT_FLUGVOGEL_CFG_KEY_LOG_CONFIG_FILE_NUM = "logFileNum"
 
 DEFAULT_FLUGVOGEL_CFG_KEY_CHANNELS = "channels"
+DEFAULT_FLUGVOGEL_CFG_KEY_USERS = "users"
+DEFAULT_FLUGVOGEL_CFG_KEY_ROLES = "roles"
+DEFAULT_FLUGVOGEL_CFG_KEY_MODULES = "modules"
 
 class FlugVogel:
     version: str = None                           # The Version of the FlugVogel
     simulate: bool = None                         # Simulate/log commands, but don't execute them
-    cfgPath : str = None                          # Path to the config file
-    tokenPath : str = None                        # Path to the token file
-    cfg : FlugConfig.FlugConfig = None            # FlugConfig instance
+    cfgPath: str = None                           # Path to the config file
+    tokenPath: str = None                         # Path to the token file
+    cfg: FlugConfig.FlugConfig = None             # FlugConfig instance
     creds: FlugCredentials.FlugCredentials = None # FlugCredentials instance
-    channels : FlugChannels.FlugChannels = None   # FlugChannels instance
+    client: FlugClient.FlugClient = None          # FlugClient instance
+    channels: FlugChannels.FlugChannels = None    # FlugChannels instance
+    users: FlugUsers.FlugUsers = None             # FlugUsers instance
+    roles: FlugRoles.FlugRoles = None             # FlugRoles instance
+    modules: FlugModules.FlugModules = None       # FlugModules instance
     _initSuccess = False # Set to `True` once initialization succeeded
 
     def __init__(self, version: str, configPath: str = DEFAULT_FLUGVOGEL_CONFIG_PATH, tokenPath: str = DEFAULT_FLUGVOGEL_TOKEN_PATH):
@@ -78,8 +90,47 @@ class FlugVogel:
         # setup the channel config
         self.channels = FlugChannels.FlugChannels(self.cfg.c()[DEFAULT_FLUGVOGEL_CFG_KEY_CHANNELS])
 
-        if not self.channels.load():
+        if not self.channels.loadCfg():
             logging.critical("Failed to load FlugVogel-Channels!")
+
+            return
+
+        # setup the user config
+        self.users = FlugUsers.FlugUsers(self.cfg.c()[DEFAULT_FLUGVOGEL_CFG_KEY_USERS])
+
+        if not self.users.loadCfg():
+            logging.critical("Failed to load FlugVogel-Users!")
+
+            return
+
+        # setup the role config
+        self.roles = FlugRoles.FlugRoles(self.cfg.c()[DEFAULT_FLUGVOGEL_CFG_KEY_ROLES])
+
+        if not self.roles.loadCfg():
+            logging.critical("Failed to load FlugVogel-Roles!")
+
+            return
+
+        # setup the client
+        intents = FlugClient.discord.Intents.default()
+        self.client = FlugClient.FlugClient(intents=intents,
+            guildId=FlugClient.discord.Object(id=self.cfg.c().get(DEFAULT_FLUGVOGEL_CFG_KEY_GUILDID))
+        )
+
+        # load and initialize modules
+        self.modules = FlugModules.FlugModules(
+            self.cfg.c()[DEFAULT_FLUGVOGEL_CFG_KEY_MODULES],
+            self.client, self.channels, self.users, self.roles
+        )
+
+        if not self.modules.loadCfg():
+            logging.critical("Failed to load FlugVogel-Roles!")
+
+            return
+
+        # initialize the modules
+        if not self.modules.initModules():
+            logging.critical("Failed to initialize modules!")
 
             return
 
@@ -102,130 +153,8 @@ class FlugVogel:
 
             return
 
-        logging.info("Hallo")
-        logging.warning("Warning")
-        logging.debug("Debug")
-
-        intents = FlugClient.discord.Intents.default()
-        client = FlugClient.FlugClient(intents=intents, guildId=FlugClient.discord.Object(id=1026808694773649449))
-
-        @client.event
-        async def on_ready():
-            print(f'Logged in as {client.user} (ID: {client.user.id})')
-            print('------')
-
-        @client.event
-        async def on_message(message: FlugClient.discord.Message):
-            id = str(message.channel.id)
-
-            if self.channels.doesChannelExist(id):
-                channel = self.channels.getchannelConfig(id)
-                
-                if channel.get("isPolitical") == True:
-                    logging.info("Reacting to a political message in '%s'!" % message.channel.name)
-
-                    if message.author.id == 580306587156217856:
-                        await message.add_reaction("🤝")
-                    elif message.author.id == 282908150993125376:
-                        await message.add_reaction("🐟")
-                    else:
-                        await message.add_reaction("<:9462pepe9:1026820740403695646>")
-            else:
-                logging.info("Message (%s) from unknown channel '%s' (%s)!" % (message.content, id, message.channel.name))
-
-        @client.tree.command()
-        async def hello(interaction: FlugClient.discord.Interaction):
-            """Says hello!"""
-            await interaction.response.send_message(f'Hi, {interaction.user.mention}')
-
-
-        @client.tree.command()
-        @FlugClient.discord.app_commands.describe(
-            first_value='The first value you want to add something to',
-            second_value='The value you want to add to the first value',
-        )
-        async def add(interaction: FlugClient.discord.Interaction, first_value: float, second_value: float):
-            """Adds two numbers together."""
-            await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
-
-        @client.tree.command()
-        @FlugClient.discord.app_commands.describe(
-            first_value='I don\'t care about what you type here, big L, go cope, lol'
-        )
-        async def dicegame(interaction: FlugClient.discord.Interaction, first_value: str):
-            """Plays a fair game of dice."""
-            member = interaction.user
-
-            if member.id == 580306587156217856: #lx00t
-                r1 = random.randint(1,5)
-                resp = "I rolled a %d. You got a %d. Fair game, champ! 🤝" % (r1, r1 + 1)
-            else:
-                r1 = random.randint(2,6)
-                resp = "I rolled a %d. You got a %d. Lol, how can you be _that_ bad? <a:pepemeltdown:1026820761840783381>" % (r1, r1 - 1)
-
-            await interaction.response.send_message(resp)
-
-
-        # The rename decorator allows us to change the display of the parameter on Discord.
-        # In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
-        # Note that other decorators will still refer to it as `text_to_send` in the code.
-        @client.tree.command()
-        @FlugClient.discord.app_commands.rename(text_to_send='text')
-        @FlugClient.discord.app_commands.describe(text_to_send='Text to send in the current channel')
-        async def send(interaction: FlugClient.discord.Interaction, text_to_send: str):
-            """Sends the text into the current channel."""
-            await interaction.response.send_message("HA! NOOOPE! Copium für Dich mein Guter! <:9462pepe9:1026820740403695646>")
-
-
-        # To make an argument optional, you can either give it a supported default argument
-        # or you can mark it as Optional from the typing standard library. This example does both.
-        @client.tree.command()
-        @FlugClient.discord.app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
-        async def joined(interaction: FlugClient.discord.Interaction, member: FlugClient.discord.Member = None):
-            """Says when a member joined."""
-            # If no member is explicitly provided then we use the command user here
-            member = member or interaction.user
-
-            # The format_dt function formats the date time into a human readable representation in the official client
-            await interaction.response.send_message(f'{member} joined {FlugClient.discord.utils.format_dt(member.joined_at)}')
-
-
-        # A Context Menu command is an app command that can be run on a member or on a message by
-        # accessing a menu within the client, usually via right clicking.
-        # It always takes an interaction as its first parameter and a Member or Message as its second parameter.
-
-        # This context menu command only works on members
-        @client.tree.context_menu(name='Show Join Date')
-        async def show_join_date(interaction: FlugClient.discord.Interaction, member: FlugClient.discord.Member):
-            # The format_dt function formats the date time into a human readable representation in the official client
-            await interaction.response.send_message(f'{member} joined at {FlugClient.discord.utils.format_dt(member.joined_at)}')
-
-
-        # This context menu command only works on messages
-        @client.tree.context_menu(name='Report to Moderators')
-        async def report_message(interaction: FlugClient.discord.Interaction, message: FlugClient.discord.Message):
-            # We're sending this response message with ephemeral=True, so only the command executor can see it
-            await interaction.response.send_message(
-                f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
-            )
-
-            # Handle report by sending it into a log channel
-            log_channel = interaction.guild.get_channel(0)  # replace with your channel id
-
-            embed = FlugClient.discord.Embed(title='Reported Message')
-            if message.content:
-                embed.description = message.content
-
-            embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-            embed.timestamp = message.created_at
-
-            url_view = FlugClient.discord.ui.View()
-            url_view.add_item(FlugClient.discord.ui.Button(label='Go to Message', style=FlugClient.discord.ButtonStyle.url, url=message.jump_url))
-
-            await log_channel.send(embed=embed, view=url_view)
-
-
-        client.run(self.creds.getToken(), log_handler=None)
+        # start the client
+        self.client.run(self.creds.getToken(), log_handler=None)
 
 if __name__ == "__main__":
     # check for parameters
---
+++ src/modules/FlugDicegame.py
@@ -0,0 +1,34 @@
+import logging
+import random
+
+import FlugChannels
+import FlugRoles
+import FlugUsers
+import FlugClient
+import FlugConfig
+
+def init(name: str, configFile: str, 
+        client: FlugClient.FlugClient,
+        channelConfig: FlugChannels.FlugChannels,
+        userConfig: FlugUsers.FlugUsers,
+        roleConfig: FlugRoles.FlugRoles):
+    # greet-message
+    logging.info("I am '%s'! I go initialized with the config file '%s'!" % (name, configFile))
+
+    # setup the dice game
+    @client.tree.command()
+    @FlugClient.discord.app_commands.describe(
+        first_value='I don\'t care about what you type here, big L, go cope, lol'
+    )
+    async def dicegame(interaction: FlugClient.discord.Interaction, first_value: str):
+        """Plays a fair game of dice."""
+        member = interaction.user
+
+        if member.id == 580306587156217856: #lx00t
+            r1 = random.randint(1,5)
+            resp = "I rolled a %d. You got a %d. Fair game, champ! 🤝" % (r1, r1 + 1)
+        else:
+            r1 = random.randint(2,6)
+            resp = "I rolled a %d. You got a %d. Lol, how can you be _that_ bad? <a:pepemeltdown:1026820761840783381>" % (r1, r1 - 1)
+
+        await interaction.response.send_message(resp)
