756978ab3e5637fd960efb3637dbf6b70e1c6792
lx00t <lx00t@nomail.nope>
2022-10-05 18:28:38 +0200

    add role and user config handling

---
+++ src/FlugRoles.py
@@ -0,0 +1,45 @@
+#
+# Role Information Handling
+#
+import FlugConfig
+import logging
+
+class FlugRoles:
+    _roleConfigPath: dict = None             # store the role config path
+    roleConfig: FlugConfig.FlugConfig = None # store the actual role config (FlugConfig instance)
+
+    def __init__(self, roleConfigPath: str):
+        self._roleConfigPath = roleConfigPath
+
+        return
+
+    def loadCfg(self) -> bool:
+        # load the config file
+        self.roleConfig = FlugConfig.FlugConfig(self._roleConfigPath, backup=True)
+
+        if self.roleConfig.load() != True:
+            logging.critical("Failed to load Role-Config from '%s'!" % self._roleConfigPath)
+
+            return False
+
+        logging.info("Role-Config loaded! It has %d entries!" % len(self.roleConfig.c().keys()))
+
+        return True
+
+    def save(self) -> bool:
+        # save the config file
+        if self.roleConfig.save() != True:
+            logging.critical("Failed to save Role-Config to '%s'!" % self._roleConfigPath)
+
+            return False
+
+        return True
+
+    def isConfigLoaded(self) -> bool:
+        return self.roleConfig != None
+
+    def isRoleKnown(self, id: str) -> bool:
+        return self.roleConfig.c().get(id, None) != None
+
+    def getroleConfig(self, id: str) -> dict:
+        return self.roleConfig.c().get(id, None)
---
+++ src/FlugUsers.py
@@ -0,0 +1,45 @@
+#
+# User Information Handling
+#
+import FlugConfig
+import logging
+
+class FlugUsers:
+    _userConfigPath: dict = None             # store the user config path
+    userConfig: FlugConfig.FlugConfig = None # store the actual user config (FlugConfig instance)
+
+    def __init__(self, userConfigPath: str):
+        self._userConfigPath = userConfigPath
+
+        return
+
+    def loadCfg(self) -> bool:
+        # load the config file
+        self.userConfig = FlugConfig.FlugConfig(self._userConfigPath, backup=True)
+
+        if self.userConfig.load() != True:
+            logging.critical("Failed to load User-Config from '%s'!" % self._userConfigPath)
+
+            return False
+
+        logging.info("User-Config loaded! It has %d entries!" % len(self.userConfig.c().keys()))
+
+        return True
+
+    def save(self) -> bool:
+        # save the config file
+        if self.userConfig.save() != True:
+            logging.critical("Failed to save User-Config to '%s'!" % self._userConfigPath)
+
+            return False
+
+        return True
+
+    def isConfigLoaded(self) -> bool:
+        return self.userConfig != None
+
+    def isUserKnown(self, id: str) -> bool:
+        return self.userConfig.c().get(id, None) != None
+
+    def getUserConfig(self, id: str) -> dict:
+        return self.userConfig.c().get(id, None)
