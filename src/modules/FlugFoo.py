020dfad8ea1336e918cf257a550adc94bf1cf289
alex <alex@fak4.tu>
2022-10-07 13:21:15 +0200

    Deleted report feature from FLugFoo.py

--- src/modules/FlugFoo.py
+++ src/modules/FlugFoo.py
@@ -99,30 +99,6 @@ class FlugFoo(modules.FlugModule.FlugModule):
             # The format_dt function formats the date time into a human readable representation in the official client
             await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
 
-
-        # This context menu command only works on messages
-        @self.client.tree.context_menu(name='Report to Moderators')
-        async def report_message(interaction: discord.Interaction, message: discord.Message):
-            # We're sending this response message with ephemeral=True, so only the command executor can see it
-            await interaction.response.send_message(
-                f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
-            )
-
-            # Handle report by sending it into a log channel
-            log_channel = interaction.guild.get_channel(0)  # replace with your channel id
-
-            embed = discord.Embed(title='Reported Message')
-            if message.content:
-                embed.description = message.content
-
-            embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-            embed.timestamp = message.created_at
-
-            url_view = discord.ui.View()
-            url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
-
-            await log_channel.send(embed=embed, view=url_view)
-
         return True
 
 CLASS = FlugFoo
