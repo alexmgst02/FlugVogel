31fc0f36eaa4071594e111f20973005fbc194ea1
lx00t <115373255+tx0c3@users.noreply.github.com>
2022-10-09 00:09:50 +0200

    adapt to permission system and extend

--- src/modules/FlugGhostDetector.py
+++ src/modules/FlugGhostDetector.py
@@ -10,19 +10,48 @@ import FlugChannels
 import FlugRoles
 import FlugUsers
 import FlugConfig
+import FlugPermissions
 
+# config keys
 DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_THRESHOLD = "threshold"
 DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_ONLY_PINGS = "onlyPings"
-DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_CONFIG_PERMITTED_ROLES = "configPermittedRoles"
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS = "permissions"
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_GHOST_PING_LOG_SIZE = "ghostPingLogSize"
 
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_ZEIGE_GEISTER_PINGS_OTHER_USER = "zeige_geister_pings_other_user"
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_SET_GHOST_DETECTOR_CONFIG = "set_ghost_detector_config"
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_GET_GHOST_DETECTOR_CONFIG = "get_ghost_detector_config"
+
+# config defaults
 DEFAULT_FLUGVOGEL_GHOSTDETECTOR_TRESHOLD = 30
 DEFAULT_FLUGVOGEL_GHOSTDETECTOR_ONLYPINGS = False
+DEFAULT_FLUGVOGEL_GHOSTDETECTOR_GHOST_PING_LOG_SIZE = 20
+
+# ghost ping log keys
+DEFAULT_FLUGVOGEL_GHOSTPING_LOG_MENTION = "mention"
+DEFAULT_FLUGVOGEL_GHOSTPING_LOG_ID = "id"
+DEFAULT_FLUGVOGEL_GHOSTPING_LOG_TS = "ts"
 
 class FlugGhostDetector(modules.FlugModule.FlugModule):
     logChannel: discord.abc.GuildChannel
     logChannelId: int = None
     onlyPings: bool = None
     treshold: int = None
+    ghostPingLogSize: int = None
+    permissions: FlugPermissions.FlugPermissions = None
+
+
+    #
+    # GhostPingLog which stores ghost pings so that users can check for it later; format:
+    # {
+    #   "<PINGED_USER_ID>": [
+    #       ("<PINGING_USER_NAME1>", "<PINGING_USER_ID1>"),
+    #       ...
+    #   ],
+    #   ...
+    # }
+    #
+    ghostPingLog: dict = {}
 
     def __init__(self, moduleName: str, configFilePath: str,
             client: FlugClient.FlugClient = None,
@@ -69,6 +98,24 @@ class FlugGhostDetector(modules.FlugModule.FlugModule):
             
             if reference:
                 embed.description += "/Reference"
+
+            # store the ghostping for all mentioned users
+            for mention in message.mentions:
+                if self.ghostPingLog.get(str(mention.id), None) == None:
+                    self.ghostPingLog.update({str(mention.id): []})
+
+                # add the message to the log
+                self.ghostPingLog.get(str(mention.id)).append({
+                    DEFAULT_FLUGVOGEL_GHOSTPING_LOG_MENTION: message.author.mention,
+                    DEFAULT_FLUGVOGEL_GHOSTPING_LOG_ID: message.author.id,
+                    DEFAULT_FLUGVOGEL_GHOSTPING_LOG_TS: f"{message.created_at}"
+                })
+
+                # check whether the maximum log size is exceeded
+                if len(self.ghostPingLog.get(str(mention.id))) > self.ghostPingLogSize > 0:
+                    self.ghostPingLog.get(str(mention.id)).pop(0)
+
+                #DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_GHOST_PING_LOG_SIZE
         elif not self.onlyPings:
             embed.description = f"Ghost-Message"
         else:
@@ -94,13 +141,26 @@ class FlugGhostDetector(modules.FlugModule.FlugModule):
         else:
             logging.info(f"Config for '{self.moduleName}' has been loaded from '{self.configFilePath}'!")
 
-        #roles permitted to config using slash command
-        self.permittedRoles = self.cfg.c().get(DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_CONFIG_PERMITTED_ROLES)
-
-        if self.permittedRoles == None:
-            logging.critical(f"Config for '{self.moduleName}' doesn't contain '{DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_CONFIG_PERMITTED_ROLES}'!")
+        # initialize the permission config
+        try:
+            self.permissions = FlugPermissions.FlugPermissions(
+                self.cfg.c().get(DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS),
+                self.roles, self.users
+            )
+        except Exception as e:
+            logging.critical(f"Failed to setup permission config for {self.moduleName}!")
+            logging.exception(e)
 
             return False
+
+        # the the maximum log size
+        self.ghostPingLogSize = self.cfg.c().get(
+            DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_GHOST_PING_LOG_SIZE,
+            DEFAULT_FLUGVOGEL_GHOSTDETECTOR_GHOST_PING_LOG_SIZE
+        )
+
+        if self.ghostPingLogSize == None:
+            logging.critical(f"")
 
         # fail if no log channel is configured
         self.logChannelId = self.channels.getLogChannelId()
@@ -109,12 +169,6 @@ class FlugGhostDetector(modules.FlugModule.FlugModule):
             logging.critical(f"No ID found for the Log-Channel '{self.moduleName}'!")
 
             return False
-
-        #load role ids for slash command config
-        self.permittedRoleIds = []
-        for key, value in self.roles.roleConfig.c().items():
-            if value.get(FlugRoles.DEFAULT_FLUGVOGEL_CFG_KEY_ROLES_NAME) in self.permittedRoles:
-                self.permittedRoleIds.append(int(key))
         
         # register the event handlers
         self.client.addSubscriber('on_ready', self.get_log_channel_on_ready)
@@ -123,44 +177,134 @@ class FlugGhostDetector(modules.FlugModule.FlugModule):
         @self.client.tree.command(description="Configure the FlugGhostDetector")
         @discord.app_commands.describe(
             treshold="Deletion of messages older than the threshold in seconds will be ignored (0 = Infinite).",
-            only_pings="Only Register Pings and References",
+            only_pings="Only Register Pings and References.",
+            ghostping_log_size="Amount of ghostpings to log per user."
         )
-        async def set_ghost_detector_config(interaction: discord.Interaction, treshold: int, only_pings: bool):
-            for role in interaction.user.roles:
-                if role.id in self.permittedRoleIds:
-                    # save the changes
-                    self.cfg._cfgObj.update({DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_THRESHOLD:treshold})
-                    self.cfg._cfgObj.update({DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_ONLY_PINGS:only_pings})
-                    self.cfg.save()
+        async def set_ghost_detector_config(interaction: discord.Interaction, treshold: int, only_pings: bool, ghostping_log_size: int):
+            # check the permissions
+            perms = self.permissions.canDo(DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_SET_GHOST_DETECTOR_CONFIG, discord.user, None)
 
-                    # build an embed
-                    embed = discord.Embed(title="🚧 New Ghost Detector Config 🚧")
-                    embed.description = f"New 'treshold': {treshold}\nNew 'only_pings': {only_pings}"
+            if perms < FlugPermissions.FlugPermissions.CAN_DO_WEAK_NO:
+                await interaction.response.send_message("Sie dürfen diesen Befehl nicht benutzen! Dieser Vorfall wird gemeldet 🚔!", ephemeral=True)
 
-                    # respond to the user-only and send to the log channel
-                    await interaction.response.send_message(
-                        f"Set new 'treshold': {treshold} and new 'only_pings': {only_pings}", ephemeral=True
-                    )
-                    await self.logChannel.send(embed=embed)
-    
-                    return
+                return
 
-            await interaction.response.send_message("Du darfst das nicht nutzen!", ephemeral=True)
+            # save the changes
+            self.cfg._cfgObj.update({
+                DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_THRESHOLD:treshold,
+                DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_ONLY_PINGS:only_pings,
+                DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_GHOST_PING_LOG_SIZE:ghostping_log_size
+            })
+
+            self.cfg.save()
+
+            # build an embed
+            embed = discord.Embed(title="🚧 New Ghost Detector Config 🚧")
+            embed.description = f"New 'treshold': {treshold}\nNew 'only_pings': {only_pings}\nNew 'ghostping_log_size': {ghostping_log_size}"
+
+            # respond to the user-only and send to the log channel
+            await interaction.response.send_message(
+                f"Set new 'treshold': {treshold} and new 'only_pings': {only_pings}\nNew 'ghostping_log_size': {ghostping_log_size}", ephemeral=True
+            )
+
+            await self.logChannel.send(embed=embed)
 
         @self.client.tree.command(description="Get the FlugGhostDetector Configuration")
         async def get_ghost_detector_config(interaction: discord.Interaction):
-            # build an embed
-            for role in interaction.user.roles:
-                if role.id in self.permittedRoleIds:
-                    embed = discord.Embed(title="🚧 Current Ghost Detector Config 🚧")
-                    embed.description = json.dumps(self.cfg.c(), indent=4)
+            # check the permissions
+            perms = self.permissions.canDo(DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_GET_GHOST_DETECTOR_CONFIG, interaction.user, None)
 
-                    await interaction.response.send_message(f"Config sent to {self.logChannel.mention}",ephemeral=True);
-                    await self.logChannel.send(embed=embed)
+            if perms < FlugPermissions.FlugPermissions.CAN_DO_WEAK_NO:
+                await interaction.response.send_message("Sie dürfen diesen Befehl nicht benutzen! Dieser Vorfall wird gemeldet 🚔!", ephemeral=True)
+
+                return
+
+            # build an embed
+            embed = discord.Embed(title="🚧 Current Ghost Detector Config 🚧")
+            embed.description = json.dumps(self.cfg.c(), indent=4)
+
+            await interaction.response.send_message(f"Config sent to {self.logChannel.mention}", ephemeral=True)
+            await self.logChannel.send(embed=embed)
+
+        @self.client.tree.command(description="Erhalte eine Liste von Leuten die dich gegeisterpinged haben.")
+        @discord.app_commands.describe(
+            subjekt="Nutzer für welchen die Ghostpings ermittelt werden sollen."
+        )
+        async def zeige_geister_pings(interaction: discord.Interaction, subjekt: discord.Member = None):
+            interaction.response.defer()
+
+            # if the user wants to get ghostpings for another user, check the perms
+            if subjekt != None and subjekt.id != interaction.user.id:
+                perms = self.permissions.canDo(
+                    DEFAULT_FLUGVOGEL_GHOSTDETECTOR_CFG_PERMISSIONS_ZEIGE_GEISTER_PINGS_OTHER_USER, 
+                    interaction.user, subjekt
+                )
+
+                # if the perms don't resolve, abort
+                if perms <= FlugPermissions.FlugPermissions.CAN_DO_WEAK_NO:
+                    logging.info(
+                        f"User {interaction.user.name} ({interaction.user.id}) tried to see" +
+                        f"Ghostpings for user {subjekt.name} ({subjekt.id}) - {FlugPermissions.FlugPermissions.CAN_DO_STRINGS[perms]}"
+                    )
+
+                    await interaction.response.send_message(
+                        f"Ihnen fehlen die nötigen Befugnisse um die Geisterpings für {subjekt.mention} einzusehen! Dieser Vorfall wird gemeldet 🚔.", ephemeral=True
+                    )
 
                     return
-                    
-            await interaction.response.send_message("Du darfst das nicht nutzen!", ephemeral=True)
+
+                # set the target to the subjekt
+                target = subjekt
+            else:
+                # the calling user is the target
+                target = interaction.user
+
+            # get the pings
+            pings = self.ghostPingLog.get(str(target.id), []) 
+
+            # create an embed and list the mentions
+            embed = discord.Embed(title="👻 Geister Pings 👻")
+            
+            if len(pings) == 1:
+                embed.description = "Es liegt ein Geisterping gegen sie vor! Folgend die verantwortliche Person:\n"
+            else:
+                embed.description = f"Es liegen {len(pings)} Geisterpings gegen Sie vor!"
+
+                if len(pings) != 0:
+                    embed.description += " Folgend die verantwortlichen Personen:\n"
+                else:
+                    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+                    return
+            
+            # list all the pings
+            for ping in pings:
+                embed.description +=\
+                    f"{ping.get(DEFAULT_FLUGVOGEL_GHOSTPING_LOG_MENTION)}\
+                     ({ping.get(DEFAULT_FLUGVOGEL_GHOSTPING_LOG_ID)}) at\
+                      {ping.get(DEFAULT_FLUGVOGEL_GHOSTPING_LOG_TS)}\n"
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+        @self.client.tree.command(description="Entleere Ihr persönliches Geisterpingpostfach.")
+        async def entleere_geister_pings(interaction: discord.Interaction):
+            interaction.response.defer()
+
+            # get the pings
+            pings = self.ghostPingLog.get(str(interaction.user.id), []) 
+
+            l = len(pings)
+
+            if l == 0:
+                await interaction.response.send_message("Ihr Geisterpingpostfach ist bereits entleert 📯!", ephemeral=True)
+            else:
+                self.ghostPingLog.update({
+                    str(interaction.user.id): []
+                })
+
+                await interaction.response.send_message(f"Es wurden {l} Elemente aus ihrem Geisterpingpostfach entfernt 📯!", ephemeral=True)
+
+            return
 
         return True
 
